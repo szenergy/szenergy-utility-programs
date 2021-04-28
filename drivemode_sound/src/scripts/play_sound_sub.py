c1bee607875a1783f4e743804dcc12a9b48b392f
zsoldosm <zsoldosmarci@hotmail.com>
2021-04-28 10:35:26 +0200

    Added fixed package (drivemode_sound)

---
+++ drivemode_sound/src/scripts/play_sound_sub.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import rospy
+import autoware_msgs.msg as aw_msgs
+from playsound import playsound
+
+buf = 0
+
+def subscriber():
+    sub = rospy.Subscriber("/vehicle_status", aw_msgs.VehicleStatus, callback_fuction)
+    rospy.spin()
+
+def callback_fuction(message):
+    if (1 - message.drivemode + buf == 0):
+        playsound("/home/zsoldos/szenergy_ws/src/autonomous_sound/src/start_trimmed.mp3")
+        global buf
+        buf = 1
+    elif (2 - message.drivemode - buf == 1):
+        playsound("/home/zsoldos/szenergy_ws/src/autonomous_sound/src/stop_trimmed.mp3")
+        global buf
+        buf = 0
+    else: None
+
+if __name__ == "__main__":
+    rospy.init_node("teszt_sub")
+    subscriber()
---
+++ drivemode_sound/src/scripts/teszt_msg.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import rospy
+import autoware_msgs.msg as aw_msgs
+
+def main():
+    rospy.init_node("teszt_m", anonymous=True)
+    t_pub = rospy.Publisher("/vehicle_status", aw_msgs.VehicleStatus, queue_size=10)
+    t_msg = aw_msgs.VehicleStatus()
+    r = rospy.Rate(5)
+    i = 0
+    while(not rospy.is_shutdown()):
+        """
+        int32 MODE_MANUAL=0
+        int32 MODE_AUTO=1
+        """
+        if i > 20:
+            t_msg.drivemode = 0
+        else:
+            t_msg.drivemode = 1
+        if i > 40:
+            i = 0
+        t_msg.header.stamp = rospy.Time.now()
+        t_pub.publish(t_msg)
+	rospy.loginfo("I publish: %i"%t_msg.drivemode)
+        i += 1
+        r.sleep()
+
+if __name__=="__main__":
+    main()
