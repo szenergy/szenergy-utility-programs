902cdcf0515dfebdfa46c9ec1c4f06e1513e9c89
zsoldosm <zsoldosmarci@hotmail.com>
2021-04-29 09:46:46 +0200

    Changed path of play_sound_sub node

--- drivemode_sound/src/scripts/play_sound_sub.py
+++ drivemode_sound/src/scripts/play_sound_sub.py
@@ -12,11 +12,11 @@ def subscriber():
 
 def callback_fuction(message):
     if (1 - message.drivemode + buf == 0):
-        playsound("/home/zsoldos/szenergy_ws/src/autonomous_sound/src/start_trimmed.mp3")
+        playsound("/home/zsoldos/szenergy_ws/src/szenergy-utility-programs/drivemode_sound/src/start_trimmed.mp3")
         global buf
         buf = 1
     elif (2 - message.drivemode - buf == 1):
-        playsound("/home/zsoldos/szenergy_ws/src/autonomous_sound/src/stop_trimmed.mp3")
+        playsound("/home/zsoldos/szenergy_ws/src/szenergy-utility-programs/drivemode_sound/src/stop_trimmed.mp3")
         global buf
         buf = 0
     else: None
