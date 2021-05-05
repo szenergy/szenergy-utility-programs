3c5621f590fd4ad36b6ecfc75855b98e984a304e
zsoldosm <zsoldosmarci@hotmail.com>
2021-05-05 10:46:34 +0200

    Added README, fixed path

---
+++ drivemode_sound/README.md
@@ -0,0 +1,9 @@
+#	Scripts
+
+- 'tesztmsg.py': Simple python script, which changes "vehicle_status"
+- 'playsoundsub.py': Simple python script, which plays sound if "vehicle_status" changed
+
+#	Required modules
+
+- 'playsound module': Cross platform module, that can play audio files.
+  Simple install with pip (pip install playsound).
--- drivemode_sound/src/scripts/play_sound_sub.py
+++ drivemode_sound/src/scripts/play_sound_sub.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python
 
 import rospy
+import rospkg
 import autoware_msgs.msg as aw_msgs
 from playsound import playsound
-
-buf = 0
 
 def subscriber():
     sub = rospy.Subscriber("/vehicle_status", aw_msgs.VehicleStatus, callback_fuction)
     rospy.spin()
 
 def callback_fuction(message):
+    global buf
     if (1 - message.drivemode + buf == 0):
-        playsound("/home/zsoldos/szenergy_ws/src/szenergy-utility-programs/drivemode_sound/src/start_trimmed.mp3")
-        global buf
+        playsound(rospack.get_path("drivemode_sound") + "/src/start_trimmed.mp3")
         buf = 1
     elif (2 - message.drivemode - buf == 1):
-        playsound("/home/zsoldos/szenergy_ws/src/szenergy-utility-programs/drivemode_sound/src/stop_trimmed.mp3")
-        global buf
+        playsound(rospack.get_path("drivemode_sound") + "/src/stop_trimmed.mp3")
         buf = 0
     else: None
 
 if __name__ == "__main__":
-    rospy.init_node("teszt_sub")
+    buf = 0
+    rospack = rospkg.RosPack()
+    rospy.init_node("drivemode_sub")
     subscriber()
