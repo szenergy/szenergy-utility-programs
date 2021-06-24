4ce8f6bfdc4e46b5fa6407c7460ae3f51c5f6c7b
horverno <horverno@gmail.com>
2021-06-24 11:39:03 +0200

    Fixed novatel sound and added a launch file

--- drivemode_sound/src/scripts/velotype_sound.py
+++ drivemode_sound/src/scripts/velotype_sound.py
@@ -2,23 +2,31 @@
 
 import rospy
 import rospkg
-import novatel_gps_msgs as nova_msgs
+try:
+    import novatel_gps_msgs.msg as nova_msgs
+except:
+    print("Download and catkin build novatel_gps_msgs")
 from playsound import playsound
 
-fixed = 0
+# rtk_fstate true if in fixed rtk state, prev is the previous state
+rtk_fstate = False; prev_rtk_fstate = False
 
 def subscriber():
     rospy.Subscriber("/velocity_type", nova_msgs.NovatelVelocity, callback_fuction)
     rospy.spin()
 
 def callback_fuction(message):
-    global fixed
-    if (message.velocity_type == 'INS_RTKFIXED') and (fixed == 0):
-        playsound(rospack.get_path("drivemode_sound") + "/src/rtk_fixed.mp3")
-        fixed = 1
-    elif (message.velocity_type != 'INS_RTKFIXED') and (fixed == 1):
-        playsound(rospack.get_path("drivemode_sound") + "/src/rtk_not_fixed.mp3")
-        fixed = 0
+    global rtk_fstate, prev_rtk_fstate
+    if (message.velocity_type == 'INS_RTKFIXED'):
+        rtk_fstate = True
+    elif (message.velocity_type != 'INS_RTKFIXED'):
+        rtk_fstate = False
+    if rtk_fstate != prev_rtk_fstate:
+        if rtk_fstate == True:
+            playsound(rospack.get_path("drivemode_sound") + "/src/rtk_fixed.mp3")
+        else:
+            playsound(rospack.get_path("drivemode_sound") + "/src/rtk_not_fixed.mp3")
+        prev_rtk_fstate = rtk_fstate
 
 if __name__ == "__main__":
     rospack = rospkg.RosPack()
