1df05ab95214768e812c898d533a25a0fc63b337
horverno <horverno@gmail.com>
2021-09-16 14:39:08 +0200

    Added script for changing bg for the demo

---
+++ ros_guis/img/bglight1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+gsettings set org.gnome.desktop.background picture-options 'centered'
+gsettings set org.gnome.desktop.background primary-color "#FFFFFF"
+f1="$(pwd)/sze_ffffff.png"
+echo "Setting new background"
+echo $f1
+gsettings set org.gnome.desktop.background picture-uri $f1
