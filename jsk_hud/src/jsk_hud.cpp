02ff6024894350e842b611eda5e1f71a221e409a
László Csaplár <csapplaci@gmail.com>
2023-04-04 15:37:10 +0200

    feat: reference values on graphs fix: separate gps status string fix: package dependency list fix: minor layout misalignments

--- jsk_hud/src/jsk_hud.cpp
+++ jsk_hud/src/jsk_hud.cpp
@@ -44,8 +44,6 @@ ros::Publisher pub_s_ref;               //speed reference (ctrlCmd)
 
 const std::string oktext = "OK";
 const std::string notoktext = "X";
-const std::string okspace = "  ";
-const std::string notokspace = " ";
 
 struct topok
 {
@@ -57,7 +55,6 @@ struct topok
     std::string outmsg;         //output text (if not okonly)
     ros::Time ts;               //time stamp
     bool ok = false;            //status
-    bool okonly = true;         //view only status (bool)
 
     topok(std::string name_in, std::string tn_in)
     {
@@ -75,21 +72,19 @@ void cb_mtxt    (const std_msgs::String &data_in)                       {mid =  
 void cb_stxt    (const std_msgs::String &data_in)                       {smt =  data_in.data;               stxt.data = "mission ID: " + mid + "\n" + smt;}
 
 void cb_model   (const visualization_msgs::Marker &data_in)             {status[0].ts = ros::Time::now();}
-void cb_ouster  (const pcl::PCLPointCloud2ConstPtr &data_in)            {status[1].ts = ros::Time::now();}
-void cb_stop    (const geometry_msgs::PoseStamped &data_in)             {status[2].ts = ros::Time::now();}
-void cb_park    (const geometry_msgs::PoseStamped &data_in)             {status[3].ts = ros::Time::now();}
-void cb_occup   (const nav_msgs::OccupancyGrid &data_in)                {status[4].ts = ros::Time::now();}
-void cb_lapoint (const geometry_msgs::PoseStamped &data_in)             {status[5].ts = ros::Time::now();}
-void cb_traj	(const visualization_msgs::MarkerArray &data_in)        {status[6].ts = ros::Time::now();}
-void cb_angle   (const std_msgs::Float32 &data_in)                      {status[7].ts = ros::Time::now();}
-void cb_speed   (const std_msgs::Float32 &data_in)                      {status[8].ts = ros::Time::now();}
-void cb_ref     (const autoware_msgs::ControlCommand &data_in)          {status[9].ts = ros::Time::now();
+void cb_stop    (const geometry_msgs::PoseStamped &data_in)             {status[1].ts = ros::Time::now();}
+void cb_park    (const geometry_msgs::PoseStamped &data_in)             {status[2].ts = ros::Time::now();}
+void cb_occup   (const nav_msgs::OccupancyGrid &data_in)                {status[3].ts = ros::Time::now();}
+void cb_lapoint (const geometry_msgs::PoseStamped &data_in)             {status[4].ts = ros::Time::now();}
+void cb_traj	(const visualization_msgs::MarkerArray &data_in)        {status[5].ts = ros::Time::now();}
+void cb_angle   (const std_msgs::Float32 &data_in)                      {status[6].ts = ros::Time::now();}
+void cb_speed   (const std_msgs::Float32 &data_in)                      {status[7].ts = ros::Time::now();}
+void cb_ref     (const autoware_msgs::ControlCommand &data_in)          {status[8].ts = ros::Time::now();
                                                                             a_temp.data = data_in.steering_angle * rtod;
                                                                             pub_a_ref.publish(a_temp);
                                                                             s_temp.data = data_in.linear_velocity;
                                                                             pub_s_ref.publish(s_temp);   }
-void cb_gps     (const geometry_msgs::PoseStamped &data_in)             {status[10].ts = ros::Time::now();}
-void cb_gps_s   (const std_msgs::String &data_in)                       {status[11].outmsg = "<span style=\"color: yellow;\">" + data_in.data + "</span>";}
+void cb_gps     (const geometry_msgs::PoseStamped &data_in)             {status[9].ts = ros::Time::now();}
 
 void timerCallback(const ros::TimerEvent& event)
 {
@@ -130,32 +125,26 @@ int main(int argc, char **argv)
     ros::Timer timer = nh.createTimer(ros::Duration(freq), timerCallback);
 
     status.push_back(topok("model", "/szemission_3d/szemission_marker"));
-    status.push_back(topok("ouster", "/os_cloud_node/points"));
-    status.push_back(topok("stop line", "/stop_line_pose"));
-    status.push_back(topok("park-area", "/park_goal_pose"));
+    status.push_back(topok("stop l. pose", "/stop_line_pose"));
+    status.push_back(topok("parking pose", "/park_goal_pose"));
     status.push_back(topok("occup map", "/occupancy_map"));
     status.push_back(topok("look fwd p", "/lookAheadPoint"));
     status.push_back(topok("trajectory", "/polynomial_trajectory"));
-    status.push_back(topok("steer ang", "/wheel_angle_deg"));
-    status.push_back(topok("speed", "/vehicle_speed_kmph"));
+    status.push_back(topok("steer angle", "/wheel_angle_deg"));
+    status.push_back(topok("veh. speed", "/vehicle_speed_kmph"));
     status.push_back(topok("ctrl cmd", "/ctrlCmd"));
-    status.push_back(topok("gps", "/current_pose"));
-    status.push_back(topok("gps status", "/gps/duro/status_string"));
+    status.push_back(topok("gps ↴", "/current_pose"));
 
-    status[ 0].s = (nh.subscribe(status[ 0].tn, 1, cb_model));
-    status[ 1].s = (nh.subscribe(status[ 1].tn, 1, cb_ouster));
-    status[ 2].s = (nh.subscribe(status[ 2].tn, 1, cb_stop));
-    status[ 3].s = (nh.subscribe(status[ 3].tn, 1, cb_park));
-    status[ 4].s = (nh.subscribe(status[ 4].tn, 1, cb_occup));
-    status[ 5].s = (nh.subscribe(status[ 5].tn, 1, cb_lapoint));
-    status[ 6].s = (nh.subscribe(status[ 6].tn, 1, cb_traj));
-    status[ 7].s = (nh.subscribe(status[ 7].tn, 1, cb_angle));
-    status[ 8].s = (nh.subscribe(status[ 8].tn, 1, cb_speed));
-    status[ 9].s = (nh.subscribe(status[ 9].tn, 1, cb_ref));
-    status[10].s = (nh.subscribe(status[10].tn, 1, cb_gps));
-    status[11].s = (nh.subscribe(status[11].tn, 1, cb_gps_s));
-
-    status[11].okonly = false;
+    status[0].s = (nh.subscribe(status[0].tn, 1, cb_model));
+    status[1].s = (nh.subscribe(status[1].tn, 1, cb_stop));
+    status[2].s = (nh.subscribe(status[2].tn, 1, cb_park));
+    status[3].s = (nh.subscribe(status[3].tn, 1, cb_occup));
+    status[4].s = (nh.subscribe(status[4].tn, 1, cb_lapoint));
+    status[5].s = (nh.subscribe(status[5].tn, 1, cb_traj));
+    status[6].s = (nh.subscribe(status[6].tn, 1, cb_angle));
+    status[7].s = (nh.subscribe(status[7].tn, 1, cb_speed));
+    status[8].s = (nh.subscribe(status[8].tn, 1, cb_ref));
+    status[9].s = (nh.subscribe(status[9].tn, 1, cb_gps));
 
     ttxt.data = "TOPIC STATUS:\n\n";
     for (int i=0; i<status.size(); i++) ttxt.data += status[i].name + "\n";
@@ -176,6 +165,7 @@ int main(int argc, char **argv)
     s_temp.data = 0.0;
     pub_s_ref.publish(s_temp);
     pub_ttxt.publish(ttxt);
+    pub_stxt.publish(stxt);
 
     ros::spin();
 }
